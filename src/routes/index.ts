fe0626d02d03f9ea46f8b9d09f425af2584a087c
Igor <chizzgar@gmail.com>
2026-02-09 15:46:56 +0000

    done 1

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 
 import { healthRouter } from "./health";
+import { usersRouter } from "./users";
 
 export const apiRouter = Router();
 
 apiRouter.use(healthRouter);
+apiRouter.use(usersRouter);
